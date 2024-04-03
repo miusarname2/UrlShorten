88bf0c31daa282705234f5aa2af147bf8eeac16e
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2024-04-03 18:47:57 -0500

    feat: :fire: I removed all the code, and proceeded to do it the traditional way.

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { con } from "./connection.db";
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
-}
-bootstrap();

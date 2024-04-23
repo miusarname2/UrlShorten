786c2225ccf9ef3c5ff02f986769402ef88f808e
miusarname2 <104049116+miusarname@users.noreply.github.com>
2024-04-23 18:21:37 -0500

    feat: :sparkles: Added the link driver to manage the behaviour of the different routes.

---
+++ app/link/aplication/link.controller.ts
@@ -0,0 +1,35 @@
+import { createLink,getAllLinks,getLinkById } from "../infrastructure/repository/link.js";
+import shortId from "shortid";
+import { Request,Response } from "express";
+
+export async function getLinks(req:Request | any,res:Response): Promise<Response<any, Record<string, any>>> {
+    try {
+        let links = await getAllLinks();
+        return res.status(200).json({message : "All Correct",status:200,data:links});
+    } catch (error:any) {
+        console.log(error);
+        return res.status(500).json({message : "Hubo un error",status:500});
+    }  
+}
+
+export async function createLinks(req:Request | any ,res:Response): Promise<Response<any, Record<string, any>>> {
+    try {
+        const uniqueId =shortId.generate();
+        req.body.refernce = uniqueId;
+        let link = await createLink(req.body);
+        return res.status(201).json({message:"Created succesful",status:201,link:link,reference : uniqueId});
+    } catch (error:any) {
+        console.log(error);
+        return res.status(501).json({message:"Failed to Create",status:501});
+    }
+}
+
+export async function getLinkByIds(req:Request | any,res:Response): Promise<Response<any, Record<string, any>>> {
+    try {
+        let link = await getLinkById(req.params.id);
+        return res.status(200).json({message:"sucess",data:link});
+    } catch (error:any) {
+        console.log(error);
+        return res.status(404).json({messsage:"not found",status:404})
+    }
+}

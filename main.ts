35c894d42e94a485d06b37dce3391f6d96e4a0fa
miusarname2 <104049116+miusarname@users.noreply.github.com>
2024-04-23 18:23:46 -0500

    feat: :sparkles: Added the main file, with everything needed to create, get and get by id

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from 'dotenv';
+import { link } from "./app/link/aplication/link.routes.js";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
+app.use('/link',link);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);

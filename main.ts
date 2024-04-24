3d0d6359e759983822e4851f0c7f8e4ba3e1f317
miusarname2 <104049116+miusarname@users.noreply.github.com>
2024-04-24 14:07:15 -0500

    feat: :sparkles: CORS policy management is instantiated.

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { link } from "./app/link/aplication/link.routes.js";
 
 dotenv.config();
@@ -8,6 +9,7 @@ const app: Express = express();
 const port: string | number=  process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({origin:"*"}));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
